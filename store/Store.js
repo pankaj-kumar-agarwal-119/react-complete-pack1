f74d1b732ae5f58b6f605c1523c775260173459a
Pankaj Kumar Agarwal <pankaj.7613@gmail.com>
2024-06-15 22:00:05 +0530

    all html files

--- store/Store.js
+++ store/Store.js
@@ -50,67 +50,74 @@ export class Store {
     }
     //Customet management
     addCustomer(customerId, name, email) {
-      if(this.#customers.has(customerId)){
-        console.log(`customer with id ${customerId} already exist!`);
-      }
-      else{
-        this.#customers.set(customerId,{name,email});
-        console.log(`customer with id ${customerId} added`);
-      }
+        if (this.#customers.has(customerId)) {
+            console.log(`customer with id ${customerId} already exist!`);
+        }
+        else {
+            this.#customers.set(customerId, { name, email });
+            console.log(`customer with id ${customerId} added`);
+        }
     }
     removeCustomer(customerId) {
-       if(this.#customers.has(customerId)){
-        this.#customers.delete(customerId);
-       }
-       else{
-        console.log(`Customer with id ${customerId} not found!`);
-       }
+        if (this.#customers.has(customerId)) {
+            this.#customers.delete(customerId);
+        }
+        else {
+            console.log(`Customer with id ${customerId} not found!`);
+        }
     }
     listCustomers() {
-     if(this.#customers.size==0){
-        console.log(`No customer record found`);
-     }
-     else{
-        this.#customers.forEach(({customerId,name,email})=>{
-            console.log(`${customerId} | ${name} | ${email}`);
-        })
-     }
+        if (this.#customers.size == 0) {
+            console.log(`No customer record found`);
+        }
+        else {
+            this.#customers.forEach(({ customerId, name, email }) => {
+                console.log(`${customerId} | ${name} | ${email}`);
+            })
+        }
     }
     placeOrder(orderId, customerId, productIds) {
-      if(this.#orders.has(orderId)){
-        console.log(`order with id ${orderId} already exist!`);
-      }
-      else if(!this.#customers.has(customerId)){
-       console.log(`Customer with id ${customerId} not found!`);
-      }
-      else{
-        const orderProducts = productIds.filter((productId)=>{//productId of my order
-            return this.#products.has(productId);
-        })
-        if(orderProducts.length != productIds.length){//productIds are actual products in my order cart + post filter order items
-       console.log(`Some of the items in your order not found`);
+        if (this.#orders.has(orderId)) {
+            console.log(`order with id ${orderId} already exist!`);
         }
-        else{
-            this.#orders.add({orderId,customerId,products : orderProducts},date : new Date());
-            console.log(`Order ${orderId} placed successfully!`);
+        else if (!this.#customers.has(customerId)) {
+            console.log(`Customer with id ${customerId} not found!`);
         }
+        else {
+            const orderProducts = productIds.filter((productId) => {//productId of my order
+                return this.#products.has(productId);
+            })
+            if (orderProducts.length != productIds.length) {//productIds are actual products in my order cart + post filter order items
+                console.log(`Some of the items in your order not found`);
+            }
+            else {
+                this.#orders.add({ orderId, customerId, products: orderProducts, date: new Date() });
+                console.log(`Order ${orderId} placed successfully!`);
+            }
 
-      }
+        }
     }
 
     listOrders() {
-      if(this.#orders.size==0){
-        console.log(`No order placed`);
-      }
+        if (this.#orders.size == 0) {
+            console.log(`No order placed`);
+        }
+        else {
+            this.#orders.forEach(({ orderId, customerId, products, date }) => {
+                console.log(`
+            Order ID : ${orderId} , Customer Id : ${customerId} , Products : ${products.join(', Date : ${date}')}
+            `);
+            })
+        }
     }
     getproduct(productId) {
-
+        return this.#products.get(productId);
     }
     getCustomer(customerId) {
-
+        return this.#customers.get(customerId);
     }
     getOrder(orderId) {
-
+        return this.#orders.get(orderId);
     }
 }
 
